ffe9a26e431fad385b074969ff83e11ad9dda89b
Calvin Leung <calvinhyleung@outlook.com>
2020-06-28 03:04:23 +0800

    submission revision

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,8 @@ router.get('/dashboard', ensureAuth, async (req, res) => {
         const logs = await Logs.find({user: req.user.id}).lean().sort({scannedAt: 'desc'});
         res.render('dashboard', {
         name: req.user.firstName,
+        image: req.user.image,
+        displayName: req.user.displayName,
         logs
         });
     } catch (error) {
